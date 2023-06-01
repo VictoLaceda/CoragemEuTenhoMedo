fe4786a8baa67f6873fe18a9ea5211417017f8fc
VictoLaceda <73712733+VictoLaceda@users.noreply.github.com>
2023-06-01 19:41:11 -0300

    004 odeio o numero
    agora ai ta inserido os monstros
    ainda falta inserir as chave estreangeira

--- ProjetoSalasVazias.sql
+++ ProjetoSalasVazias.sql
@@ -1,12 +1,12 @@
 CREATE TABLE nv_perigo (
     id SERIAL NOT NULL,
-    nome VARCHAR(20) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE raridade (
     id SERIAL NOT NULL,
-    nome VARCHAR(10) NOT NULL,
+    nome VARCHAR(50) NOT NULL,
     PRIMARY KEY (id)
 );
 
@@ -27,14 +27,14 @@ alter TABLE monstrobicho (
 
 create table descricao(
   id SERIAL NOT NULL,
-  nome VARCHAR(300) NOT NULL,
+  nome VARCHAR(650) NOT NULL,
   monstrobicho_id INTEGER REFERENCES monstrobicho(id),
   PRIMARY KEY (id)
 );
 
 create table aparencia(
   id SERIAL NOT NULL,
-  aparencia VARCHAR(300) NOT NULL,
+  aparencia VARCHAR(150) NOT NULL,
   monstrobicho_nome INTEGER REFERENCES monstrobicho(id),
   PRIMARY KEY (id)
 );
@@ -42,20 +42,15 @@ create table aparencia(
 create table niveis_lugares(
 	id serial not NULL,
   	nome VARCHAR(20),
-  	subtitulo varchar(150),
-	monstrobicho_id INTEGER REFERENCES monstrobicho(id),
-  	nv_perigo_id INTEGER REFERENCES nv_perigo(id)
+  	subtitulo varchar(100),
+	monstrobicho_id INTEGER REFERENCES monstrobicho(id)
 );
 
 create table portais(
+	id serial not NULL,
+  	nome VARCHAR(20),
+  	subtitulo varchar(30),
 	niveis_lugares_fk INTEGER REFERENCES niveis_lugares(id)
-);
-
-create table locais_monstros(
-  id serial not NULL,
-  nome VARCHAR(20),
-  monstrobicho_id INTEGER REFERENCES monstrobicho(id),
-  niveis_lugares_id INTEGER REFERENCES niveis_lugares(id)
 );
 
 /* INSERINDO OS LUGARES DO DIAB  */
@@ -70,26 +65,18 @@ insert into niveis_lugares(nome,subtitulo) values ('Luzes apagadas','completamen
 insert into niveis_lugares(nome,subtitulo) values ('Talassofobia','vasta extensão de água que parece se estender infinitamente, tornando-se um oceano sem vida');
 insert into niveis_lugares(nome,subtitulo) values ('Sistema de Cavernas','sistema de cavernas expansivo');
 
-/* E QUAIS sao as chances de ver  MONSTROS TEM NESSES NIVEIS*/
 
-INSERT INTO raridade(nome) VALUES('Desconhecido:');
-INSERT INTO raridade(nome) VALUES('Extinta:');
-INSERT INTO raridade(nome) VALUES('Quase Extinto:'); 
-INSERT INTO raridade(nome) VALUES('Raro:'); 
-INSERT INTO raridade(nome) VALUES('Incomum:'); 
-INSERT INTO raridade(nome) VALUES('Comum :');
-INSERT INTO raridade(nome) VALUES('Maioria:');
-INSERT INTO raridade(nome) VALUES('Muitos:');
+/* E QUAIS MONSTROS TEM NESSES NIVEIS*/
 
-/* GRAU DE AMEAÇA DESSES MONSTROS */
+/* E COMO IR PARA OUTROS LOCAIS
 
-insert into nv_perigo values ('Cl. 1 N agressivo');
-insert into nv_perigo values ('Cl. 2 norm N.agres');
-insert into nv_perigo values ('Cl. 3 Agressivo');
-insert into nv_perigo values ('Cl. 4 Bem agresi');
-insert into nv_perigo values ('Cl. 5 Perigoso');
-insert into nv_perigo values ('Cl. 6 Bem perigo');
-insert into nv_perigo values ('Cl. 7 Mitologico');
-INSERT INTO nv_perigo values ('Cl. poseidom');
 
-/* E COMO IR PARA OUTROS LOCAIS */
+insert 
+
+insert into 
+
+
+
+
+
+
